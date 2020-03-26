94cbbbd156b9fff54f42be01e0b4991a3efa9ecb
Dragan Pepic <d.pepic@hotmail.com>
2020-03-26 16:57:54 +0100

    Add project files.

---
+++ ImenikV2/Artikal.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImenikV2
+{
+	[Serializable]
+	class Artikal : INotifyPropertyChanged
+	{
+		public string Sifra { get; set; }
+		public string Naziv { get; set; }
+
+		private decimal ulaznaCena;
+		public decimal UlaznaCena 
+		{ 
+			get
+			{
+				return ulaznaCena;
+			}
+			set
+			{
+				ulaznaCena = value;
+				PropertyChanged.Invoke(this, new PropertyChangedEventArgs("UlaznaCena"));
+				PropertyChanged.Invoke(this, new PropertyChangedEventArgs("IzlaznaCena"));
+			}
+		}
+
+		private int marza;
+		public int Marza 
+		{ 
+			get
+			{
+				return marza;
+			}
+			set
+			{
+				marza = value;
+				PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Marza"));
+				PropertyChanged.Invoke(this, new PropertyChangedEventArgs("IzlaznaCena"));
+			}
+		}
+
+		[field: NonSerialized]
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		private int kolicina;
+		public int Kolicina 
+		{ 
+			get
+			{
+				return kolicina;
+			}
+			set
+			{
+				if (value > 0)
+				{
+					kolicina = value;
+				} else
+				{
+					kolicina = 0;
+				}
+				PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Kolicina"));
+			}
+		}
+
+		public decimal IzlaznaCena
+		{
+			get
+			{
+				return UlaznaCena * ((decimal)Marza / 100 + 1);
+			}
+		}
+
+		public override string ToString()
+		{
+			return $"{Sifra} - {Naziv}";
+		}
+
+
+	}
+}
---
+++ ImenikV2/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ImenikV2
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		ObservableCollection<Artikal> Artikli = new ObservableCollection<Artikal>();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			BinaryFormatter bf = new BinaryFormatter();
+			using (FileStream fs = new FileStream("art.dat", FileMode.Open, FileAccess.Read))
+			{
+				Artikli = bf.Deserialize(fs) as ObservableCollection<Artikal>;
+			}
+			dg.ItemsSource = Artikli;
+		}
+
+		private void ZatvaramSe(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			BinaryFormatter bf = new BinaryFormatter();
+			using (FileStream fs = new FileStream("art.dat", FileMode.Create, FileAccess.Write))
+			{
+				bf.Serialize(fs, Artikli);
+			}
+		}
+	}
+}
